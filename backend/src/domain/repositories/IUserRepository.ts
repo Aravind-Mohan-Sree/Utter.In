f1d10f5a458a7bd5a3f8285a11663388a79b3ec5
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:12:51 +0530

    feat: Update IUserRepository to include IUser for improved type safety

--- backend/src/domain/repositories/IUserRepository.ts
+++ backend/src/domain/repositories/IUserRepository.ts
@@ -1,6 +1,7 @@
 import { User } from '~entities/User';
 import { IBaseRepository } from './IBaseRepository';
+import { IUser } from '~models/UserModel';
 
-export interface IUserRepository extends IBaseRepository<User> {
-  findByEmail(email: string): Promise<User | null>;
+export interface IUserRepository extends IBaseRepository<User, IUser> {
+  // findByEmail(email: string): Promise<User | null>;
 }
