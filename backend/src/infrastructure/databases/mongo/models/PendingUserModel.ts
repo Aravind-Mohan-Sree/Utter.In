cd044573e79c22fcaf8ba29a12452ffa90247f9d
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:13:12 +0530

    feat: Update PendingUserModel schema to allow optional fields and set defaults

--- backend/src/infrastructure/databases/mongo/models/PendingUserModel.ts
+++ backend/src/infrastructure/databases/mongo/models/PendingUserModel.ts
@@ -12,14 +12,17 @@ export interface IPendingUser extends Document {
 
 const pendingUserSchema = new Schema<IPendingUser>(
   {
-    name: { type: String, required: true },
+    name: { type: String, default: null },
     email: { type: String, required: true, unique: true },
-    knownLanguages: { type: [String], required: true },
-    password: { type: String, required: true },
-    otp: { type: String },
-    createdAt: { type: Date, default: Date.now, expires: 300 },
+    knownLanguages: { type: [String], default: null },
+    password: { type: String, default: null },
+    otp: { type: String, default: null },
+    updatedAt: { type: Date, default: Date.now, expires: 120 },
   },
   { timestamps: true },
 );
 
-export const PendingUserModel = mongoose.model('pendingUsers', pendingUserSchema);
+export const PendingUserModel = mongoose.model(
+  'pendingUsers',
+  pendingUserSchema,
+);
