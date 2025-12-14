1249f305585b327749d4e8c68c5677ec180b7309
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:10:14 +0530

    feat: Add ISigninUserUseCase interface for user authentication

--- backend/src/application/interfaces/use-cases/IUserUseCase.ts
+++ backend/src/application/interfaces/use-cases/IUserUseCase.ts
@@ -1,4 +1,5 @@
 import { RegisterUserDTO } from '~/application/dtos/RegisterUserDTO';
+import { SigninUserDTO } from '~/application/dtos/SigninUserDTO';
 import { User } from '~entities/User';
 
 export interface IRegisterUserUseCase {
@@ -7,4 +8,23 @@ export interface IRegisterUserUseCase {
 
 export interface IRegisterUserFromPendingUseCase {
   execute(email: string): Promise<Partial<User>>;
+}
+
+export interface IGoogleAuthUseCase {
+  execute(
+    email: string,
+    googleId: string,
+  ): Promise<{
+    user: Partial<User>;
+    accessToken: string;
+    refreshToken: string;
+  }>;
+}
+
+export interface ISigninUserUseCase {
+  execute(data: SigninUserDTO): Promise<{
+    user: Partial<User>;
+    accessToken: string;
+    refreshToken: string;
+  }>;
 }
