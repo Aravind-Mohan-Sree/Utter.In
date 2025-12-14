93e33afb54e944b737591bb51faa315631fe85d5
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:15:28 +0530

    feat: Add error messages for authentication and validation scenarios

--- backend/src/shared/constants/errorMessage.ts
+++ backend/src/shared/constants/errorMessage.ts
@@ -1,2 +1,11 @@
 export const errorMessage = {
+  INVALID_DATA: 'Provided data is invalid',
+  OTP_EXPIRED: 'OTP expired, please try again',
+  ACCOUNT_EXISTS: 'Account already exists',
+  ACCOUNT_NOT_EXISTS: 'Account not exists',
+  BLOCKED: 'Account is blocked. Please contact Admin',
+  UNAUTHORIZED: 'Not authenticated. Please signin',
+  SESSION_EXPIRED: 'Session expired. Please signin',
+  FORBIDDEN: 'Access denied due to insufficient permission',
+  WRONG_PASSWORD: 'Wrong password',
 };
