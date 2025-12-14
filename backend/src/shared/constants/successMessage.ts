9b9b0b501450f66383e3167546423630570fc245
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:22:30 +0530

    feat: Add success messages

--- backend/src/shared/constants/successMessage.ts
+++ backend/src/shared/constants/successMessage.ts
@@ -1,2 +1,8 @@
 export const successMessage = {
+  SIGNUP_SUCCESS: 'Signup successful',
+  SIGNIN_SUCCESS: 'Signin successful',
+  SIGNOUT_SUCCESS: 'Signout successful',
+  OTP_SENDED: 'OTP sended',
+  OTP_VERIFIED: 'OTP verified',
+  PASSWORD_RESET_SUCCESS: 'Password reset successful',
 };
