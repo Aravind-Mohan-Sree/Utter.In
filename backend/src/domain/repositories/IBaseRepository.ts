50d1d6e3304387921767cda81383c1649d765caf
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:12:39 +0530

    feat: Enhance IBaseRepository interface with generic ModelSchema for improved type safety

--- backend/src/domain/repositories/IBaseRepository.ts
+++ backend/src/domain/repositories/IBaseRepository.ts
@@ -1,3 +1,20 @@
-export interface IBaseRepository<Entity> {
-  create(data: Entity): Promise<Entity | null>;
+export interface IBaseRepository<Entity, ModelSchema> {
+  create(entity: Entity): Promise<Entity>;
+  findOneById(id: string): Promise<Entity | null>;
+  findOneByField(filter: Partial<ModelSchema>): Promise<Entity | null>;
+  findAllByField(filter: Partial<ModelSchema>): Promise<Entity[] | null>;
+  updateOneById(
+    id: string,
+    entity: Partial<Entity>,
+  ): Promise<Entity | null>;
+  updateOneByField(
+    filter: Partial<ModelSchema>,
+    entity: Partial<Entity>,
+  ): Promise<Entity | null>;
+  updateAllByField(
+    filter: Partial<ModelSchema>,
+    entity: Partial<Entity>,
+  ): Promise<void | boolean>;
+  deleteOneById(id: string): Promise<void | boolean>;
+  deleteOneByField(filter: Partial<ModelSchema>): Promise<void | boolean>;
 }
