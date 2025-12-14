a3c5648b1da47da5e60fd230707c2ea9ce8f7b84
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:12:00 +0530

    feat: Implement ForgotPasswordUseCase for handling password reset requests

---
+++ backend/src/application/use-cases/user/user-management/ForgotPasswordUseCase.ts
@@ -0,0 +1,36 @@
+import { IForgotPasswordUseCase } from '~application-interfaces/use-cases/IForgotPasswordUseCase';
+import { errorMessage } from '~constants/errorMessage';
+import { IPendingUserRepository } from '~domain-repositories/IPendingUserRepository';
+import { IUserRepository } from '~domain-repositories/IUserRepository';
+import { PendingUser } from '~entities/PendingUser';
+import { BadRequestError, NotFoundError } from '~errors/HttpError';
+
+export class ForgotPasswordUseCase implements IForgotPasswordUseCase {
+  constructor(
+    private userRepo: IUserRepository,
+    private pendingUserRepo: IPendingUserRepository,
+  ) {}
+
+  async execute(email: string): Promise<void> {
+    const user = await this.userRepo.findOneByField({ email });
+
+    if (!user) throw new NotFoundError(errorMessage.ACCOUNT_NOT_EXISTS);
+
+    let pendingUser = await this.pendingUserRepo.findOneByField({ email });
+
+    if (pendingUser) {
+      const now = new Date().getTime();
+      const coolDownMs = 60 * 1000;
+      const timeDifferenceMs = now - new Date(pendingUser.updatedAt!).getTime();
+
+      if (timeDifferenceMs < coolDownMs)
+        throw new BadRequestError('Please wait 60 sec before resend');
+
+      await this.pendingUserRepo.deleteOneByField({ email });
+    }
+
+    pendingUser = new PendingUser(email, user.name);
+
+    await this.pendingUserRepo.create(pendingUser);
+  }
+}
