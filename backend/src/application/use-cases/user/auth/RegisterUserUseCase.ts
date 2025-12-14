edeec666cebede4a057a2cc73c499e4aecf8ff6b
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:11:33 +0530

    feat: Refactor RegisterUserUseCase to enhance user registration logic and error handling

--- backend/src/application/use-cases/user/auth/RegisterUserUseCase.ts
+++ backend/src/application/use-cases/user/auth/RegisterUserUseCase.ts
@@ -1,8 +1,10 @@
 import { RegisterUserDTO } from '~/application/dtos/RegisterUserDTO';
 import { IRegisterUserUseCase } from '~application-interfaces/use-cases/IUserUseCase';
+import { errorMessage } from '~constants/errorMessage';
 import { IPendingUserRepository } from '~domain-repositories/IPendingUserRepository';
 import { IUserRepository } from '~domain-repositories/IUserRepository';
 import { IHashService } from '~domain-services/IHashService';
+import { PendingUser } from '~entities/PendingUser';
 import { ConflictError, InternalServerError } from '~errors/HttpError';
 
 export class RegisterUserUseCase implements IRegisterUserUseCase {
@@ -13,18 +15,28 @@ export class RegisterUserUseCase implements IRegisterUserUseCase {
   ) {}
 
   async execute(data: RegisterUserDTO): Promise<string> {
-    const user = await this.userRepo.findByEmail(data.email);
+    const user = await this.userRepo.findOneByField({ email: data.email });
 
-    if (user) throw new ConflictError('Email already exists');
+    if (user) throw new ConflictError(errorMessage.ACCOUNT_EXISTS);
+
+    let pendingUser = await this.pendingUserRepo.findOneByField({
+      email: data.email,
+    });
+
+    if (pendingUser) {
+      await this.pendingUserRepo.deleteOneByField({ email: pendingUser.email });
+    }
 
     const hashedPassword = await this.hashService.hash(data.password);
 
-    const pendingUser = await this.pendingUserRepo.create({
-      name: data.name,
-      email: data.email,
-      knownLanguages: data.knownLanguages,
-      password: hashedPassword,
-    });
+    pendingUser = new PendingUser(
+      data.email,
+      data.name,
+      data.knownLanguages,
+      hashedPassword,
+    );
+
+    pendingUser = await this.pendingUserRepo.create(pendingUser);
 
     if (!pendingUser) throw new InternalServerError('Something went wrong');
 
