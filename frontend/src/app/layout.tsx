91effd24d09e2342b9884a04e07e85b13c26a366
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:25:54 +0530

    feat: Store integrated

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import { Poppins } from 'next/font/google';
 import './globals.css';
+import { Toaster } from 'sonner';
+import { ReduxProvider } from 'ReduxProvider';
 
 const poppins = Poppins({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
@@ -20,7 +22,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.variable} antialiased`}>{children}</body>
+      <body className={`${poppins.variable} antialiased`}>
+        <ReduxProvider>{children}</ReduxProvider>
+        <Toaster
+          toastOptions={{
+            style: {
+              color: '#FFFFFF',
+              background: '#FD637D',
+            },
+          }}
+        />
+      </body>
     </html>
   );
 }
