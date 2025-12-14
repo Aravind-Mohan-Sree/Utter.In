abf54974dee5d94ef3e09cf59842e766fc24f777
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:12:05 +0530

    feat: Implement ResetPasswordUseCase for handling password reset functionality

---
+++ backend/src/application/use-cases/user/user-management/ResetPasswordUseCase.ts
@@ -0,0 +1,24 @@
+import { IResetPasswordUseCase } from '~application-interfaces/use-cases/IForgotPasswordUseCase';
+import { IUserRepository } from '~domain-repositories/IUserRepository';
+import { IHashService } from '~domain-services/IHashService';
+import { ITokenService } from '~domain-services/ITokenService';
+import { User } from '~entities/User';
+
+export class ResetPasswordUseCase implements IResetPasswordUseCase {
+  constructor(
+    private tokenService: ITokenService,
+    private userRepo: IUserRepository,
+    private hashService: IHashService,
+  ) {}
+
+  async execute(resetToken: string, password: string): Promise<void> {
+    const payload = this.tokenService.verifyResetToken(resetToken);
+    const email = payload.email;
+    const hashedPassword = await this.hashService.hash(password);
+    const user: Partial<User> = {
+      password: hashedPassword,
+    };
+
+    await this.userRepo.updateOneByField({ email }, user);
+  }
+}
