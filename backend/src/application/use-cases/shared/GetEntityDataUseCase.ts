0124197e9e3d519192e9926e1ad568edf14317aa
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:11:01 +0530

    feat: Implement GetEntityDataUseCase for entity retrieval by ID

---
+++ backend/src/application/use-cases/shared/GetEntityDataUseCase.ts
@@ -0,0 +1,17 @@
+import { IGetEntityDataUseCase } from '~application-interfaces/use-cases/IGetEntityDataUseCase';
+import { IBaseRepository } from '~domain-repositories/IBaseRepository';
+
+export class GetEntityDataUseCase<
+  Entity,
+  ModelSchema,
+> implements IGetEntityDataUseCase<Entity> {
+  constructor(private baseRepo: IBaseRepository<Entity, ModelSchema>) {}
+
+  async getOneById(id: string): Promise<Entity | null> {
+    const entity = await this.baseRepo.findOneById(id);
+
+    if (!entity) return null;
+
+    return entity;
+  }
+}
