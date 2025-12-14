22a1d14bb10aa944da75f1ca908dc7d6226ad942
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 18:13:41 +0530

    refactor: Clean code

--- backend/src/domain/repositories/IUserRepository.ts
+++ backend/src/domain/repositories/IUserRepository.ts
@@ -3,5 +3,5 @@ import { IBaseRepository } from './IBaseRepository';
 import { IUser } from '~models/UserModel';
 
 export interface IUserRepository extends IBaseRepository<User, IUser> {
-  // findByEmail(email: string): Promise<User | null>;
+  
 }
