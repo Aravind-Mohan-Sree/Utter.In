dbf2c2ba5bf546549e222bdbfba4bc8c52dc467f
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:14:30 +0530

    feat: Implement OtpController for OTP verification and resend functionality

---
+++ backend/src/presentation/controllers/user/OtpController.ts
@@ -0,0 +1,46 @@
+import { NextFunction, Request, Response } from 'express';
+import { ISendOtpUseCase, IVerifyOtpUseCase } from '~application-interfaces/use-cases/IOtpUseCase';
+import { IRegisterUserFromPendingUseCase } from '~application-interfaces/use-cases/IUserUseCase';
+import { httpStatusCode } from '~constants/httpStatusCode';
+import { successMessage } from '~constants/successMessage';
+import { logger } from '~logger/logger';
+
+export class OtpController {
+  constructor(
+    private verifyOtp: IVerifyOtpUseCase,
+    private sendOtp: ISendOtpUseCase,
+    private registerUserFromPending: IRegisterUserFromPendingUseCase,
+  ) {}
+
+  verify = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { otp, email } = req.body;
+
+      await this.verifyOtp.execute(email, otp);
+
+      const user = await this.registerUserFromPending.execute(email);
+
+      res
+        .status(httpStatusCode.OK)
+        .json({ message: successMessage.SIGNUP_SUCCESS, user });
+    } catch (error) {
+      logger.error(error);
+      next(error);
+    }
+  };
+
+  resend = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email } = req.body;
+
+      await this.sendOtp.execute(email);
+
+      res
+        .status(httpStatusCode.OK)
+        .json({ message: successMessage.OTP_SENDED });
+    } catch (error) {
+      logger.error(error);
+      next(error);
+    }
+  };
+}
