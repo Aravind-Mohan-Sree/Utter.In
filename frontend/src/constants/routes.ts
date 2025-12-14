1c83d516e8013fb468efc56775d6abe421b5387c
Aravind M S <mailuraravindms@gmail.com>
2025-12-14 16:37:44 +0530

    feat: Add centralized routes

---
+++ frontend/src/constants/routes.ts
@@ -0,0 +1,22 @@
+export const API_ROUTES = {
+  USER: {
+    SIGNUP: '/user/signup',
+    VERIFY_OTP: '/user/verify-otp',
+    VERIFY_EMAIL: '/user/verify-email',
+    RESEND_OTP: '/user/resend-otp',
+    FORGOT_PASSWORD_OTP_VERIFY: '/user/forgot-password-otp-verify',
+    RESET_PASSWORD: '/user/reset-password',
+    SIGNIN: '/user/signin',
+    GOOGLE_SIGNIN: '/user/auth/google',
+  },
+  TUTOR: {
+    SIGNUP: '/tutor/signup',
+    VERIFY_OTP: '/tutor/verify-otp',
+    VERIFY_EMAIL: '/tutor/verify-email',
+    RESEND_OTP: '/tutor/resend-otp',
+    FORGOT_PASSWORD_OTP_VERIFY: '/tutor/forgot-password-otp-verify',
+    RESET_PASSWORD: '/tutor/reset-password',
+    SIGNIN: '/tutor/signin',
+    GOOGLE_SIGNIN: '/tutor/auth/google',
+  },
+};
