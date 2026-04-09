7843016a0b4bb185575078f19be733fc2fcf9faa
Aravind M S <mailuraravindms@gmail.com>
2026-04-09 18:26:32 +0530

    ''

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,10 @@
-import { initializeAWSConfig } from './config/env.js';
+import { initializeAWSConfig, env } from './config/env.js';
 
 async function bootstrap() {
   try {
     await initializeAWSConfig();
     console.log('--- AWS CONFIG LOADED ---');
-
+    console.log(env.MONGO_CONNECTION_URI);
     await import('./app.js');
   } catch (error) {
     console.error('Failed to bootstrap application:', error);
